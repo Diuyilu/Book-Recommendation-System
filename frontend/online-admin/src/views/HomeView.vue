2d3e39c1b21786a7a97de390439ea5bea6446d6c
diuyilu <slkxyyl@126.com>
2023-09-01 18:14:29 +0800

    正式编辑前端模块，截至到解决跨域问题

---
+++ frontend/online-admin/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </main>
+</template>
--- frontend/vue-project/src/main.js
+++ frontend/vue-project/src/main.js
@@ -1,6 +1,18 @@
 import './assets/main.css'
 
+// 原代码
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+// Element Plus完整引入
+// import { createApp } from 'vue'
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+// import App from './App.vue'
+
+// const app = createApp(App)
+
+// app.use(ElementPlus)
+// app.mount('#app')
